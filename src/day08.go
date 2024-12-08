f431f1c7e782498cb5685d5b3d8b0fbddbfcd423
nburchha <niklas@burchhardt.de>
2024-12-08 11:21:23 +0000

    day08 part 2

--- src/day08.go
+++ src/day08.go
@@ -12,34 +12,43 @@ type Coord struct {
 	Y int
 }
 
-func checkAntiNode(antinode, b Coord, rows, cols int) bool {
+func checkAntiNode1(antinode, b Coord, rows, cols int) bool {
 	return antinode.X >= 0 && antinode.X < cols && antinode.Y >= 0 && antinode.Y < rows && antinode != b
+}
+
+func checkAntiNode2(antinode, b Coord, rows, cols int) bool {
+	return antinode.X >= 0 && antinode.X < cols && antinode.Y >= 0 && antinode.Y < rows
 }
 
 func validAntiNode(a, b Coord, rows, cols int) (int, []Coord) {
 	var delta Coord
 	delta.X = b.X - a.X
 	delta.Y = b.Y - a.Y
-	// fmt.Println("coords:", a, b)
-	antinode1 := Coord{X: b.X + delta.X, Y: b.Y + delta.Y}
-	antinode2 := Coord{X: a.X - delta.X, Y: a.Y - delta.Y}
 
-	var count int
-	res := []Coord{
-		{X: -1, Y: -1},
-		{X: -1, Y: -1},
+	count := 0
+	res := make([]Coord, 0)
+	res = append(res, a)
+	res = append(res, b)
+	for i := 1; i < 1000; i++ {
+		antinode := Coord{X: (b.X + delta.X*i), Y: (b.Y + delta.Y*i)}
+		if checkAntiNode2(antinode, a, rows, cols) {
+			fmt.Println("valid antinode:", antinode)
+			count++
+			res = append(res, antinode)
+		}
+		antinode = Coord{X: (b.X - delta.X*i), Y: (b.Y - delta.Y*i)}
+		if checkAntiNode2(antinode, a, rows, cols) {
+			fmt.Println("valid antinode:", antinode)
+			count++
+			res = append(res, antinode)
+		}
+		antinode = Coord{X: (a.X + delta.X*i), Y: (a.Y + delta.Y*i)}
+		if checkAntiNode2(antinode, b, rows, cols) {
+			fmt.Println("valid antinode:", antinode)
+			count++
+			res = append(res, antinode)
+		}
 	}
-	if checkAntiNode(antinode1, a, rows, cols) {
-		fmt.Println("valid antinode:", antinode1)
-		count++
-		res[0] = antinode1
-	}
-	if checkAntiNode(antinode2, b, rows, cols) {
-		fmt.Println("valid antinode:", antinode2)
-		count++
-		res[1] = antinode2
-	}
-	// fmt.Println("count",count)
 	return count, res
 }
 
@@ -49,7 +58,9 @@ func inBoundAntiNodes(antennas []Coord, rows, cols int) (int, []Coord) {
 	for i, a := range antennas {
 		for j := i + 1; j < len(antennas); j++ {
 			b := antennas[j]
-			if a == b { continue }
+			if a == b {
+				continue
+			}
 			tmp, positions := validAntiNode(a, b, rows, cols)
 			count += tmp
 			if count != 0 {
@@ -57,27 +68,25 @@ func inBoundAntiNodes(antennas []Coord, rows, cols int) (int, []Coord) {
 			}
 		}
 	}
-	// for _, line := range grid {
-	// 	fmt.Println(line)
-	// }
 	return count, antiNodes
 }
 
 func removeDuplicates(coords []Coord) []Coord {
-    uniqueMap := make(map[Coord]struct{})
-    for _, coord := range coords {
-        uniqueMap[coord] = struct{}{}
-    }
+	uniqueMap := make(map[Coord]struct{})
+	for _, coord := range coords {
+		uniqueMap[coord] = struct{}{}
+	}
 
-    uniqueCoords := make([]Coord, 0, len(uniqueMap))
-    for coord := range uniqueMap {
-        uniqueCoords = append(uniqueCoords, coord)
-    }
+	uniqueCoords := make([]Coord, 0, len(uniqueMap))
+	for coord := range uniqueMap {
+		uniqueCoords = append(uniqueCoords, coord)
+	}
 
-    return uniqueCoords
+	return uniqueCoords
 }
 
 func main() {
+	// input, _ := os.ReadFile("input/testinput8")
 	input, _ := os.ReadFile("input/day08.txt")
 	split := strings.Split(strings.TrimSpace(string(input)), "\n\n")
 	fmt.Println(split[0])
@@ -99,5 +108,5 @@ func main() {
 		antiNodes = append(antiNodes, tmp1...)
 	}
 	antiNodes = removeDuplicates(antiNodes)
-	fmt.Println(len(antiNodes) - 1)
+	fmt.Println(len(antiNodes))
 }
