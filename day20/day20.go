e50fe2130f8e447d426d64a1671ef7df00a93983
nburchha <niklas@burchhardt.de>
2024-12-20 06:40:55 +0000

    day20 part2 done

--- day20/day20.go
+++ day20/day20.go
@@ -14,6 +14,141 @@ const (
 
 type point struct{r, c int}
 
+var dirs = []point{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
+
+func isTrack(ch byte) bool {
+    return ch == '.' || ch == 'S' || ch == 'E'
+}
+
+// allDistances performs a BFS from a single start point and returns a 2D slice of distances to all reachable cells.
+// If a cell is not reachable, distance will be -1.
+func allDistances(grid []string, start point) [][]int {
+    h := len(grid)
+    w := len(grid[0])
+    dist := make([][]int, h)
+    for i := range dist {
+        dist[i] = make([]int, w)
+        for j := range dist[i] {
+            dist[i][j] = -1
+        }
+    }
+
+    dist[start.r][start.c] = 0
+    q := []point{start}
+    for len(q) > 0 {
+        cur := q[0]
+        q = q[1:]
+        for _, d := range dirs {
+            nr, nc := cur.r+d.r, cur.c+d.c
+            if nr < 0 || nr >= h || nc < 0 || nc >= w {
+                continue
+            }
+            if dist[nr][nc] == -1 && isTrack(grid[nr][nc]) {
+                dist[nr][nc] = dist[cur.r][cur.c] + 1
+                q = append(q, point{nr,nc})
+            }
+        }
+    }
+    return dist
+}
+
+// solvePart2 implements the logic for part two.
+func solvePart2(grid []string, start, end point) int {
+    h := len(grid)
+    w := len(grid[0])
+
+    normalDistFromStart := allDistances(grid, start)
+    normalDistFromEnd := allDistances(grid, end)
+
+    normalTime := normalDistFromStart[end.r][end.c]
+
+    if normalTime == -1 {
+        return 0
+    }
+
+    type cheatID struct {
+        sr, sc, er, ec int
+    }
+    bestSavings := make(map[cheatID]int)
+
+    hIn := func(r, c int) bool {return r>=0 && r<h && c>=0 && c<w}
+
+    for sr := 0; sr < h; sr++ {
+        for sc := 0; sc < w; sc++ {
+            if normalDistFromStart[sr][sc] == -1 {
+                continue
+            }
+            distStart := normalDistFromStart[sr][sc]
+
+            visited := make([][][]bool, h)
+            for i := 0; i < h; i++ {
+                visited[i] = make([][]bool, w)
+                for j := 0; j < w; j++ {
+                    visited[i][j] = make([]bool, 21)
+                }
+            }
+
+            // BFS queue for cheat mode
+            type state struct {
+                r, c, steps int
+            }
+
+            visited[sr][sc][0] = true
+            queue := []state{{sr, sc, 0}}
+
+            for len(queue) > 0 {
+                cur := queue[0]
+                queue = queue[1:]
+
+                for _, d := range dirs {
+                    nr, nc := cur.r+d.r, cur.c+d.c
+                    if !hIn(nr,nc) {
+                        continue
+                    }
+                    nsteps := cur.steps + 1
+                    if nsteps > 20 {
+                        continue
+                    }
+                    if !visited[nr][nc][nsteps] {
+                        visited[nr][nc][nsteps] = true
+                        queue = append(queue, state{nr,nc,nsteps})
+                    }
+
+                    // Potential cheat end conditions:
+                    // If we reached 'E':
+                    if grid[nr][nc] == 'E' {
+                        cheatedTime := distStart + nsteps
+                        saving := normalTime - cheatedTime
+                        cid := cheatID{sr, sc, nr, nc}
+                        if saving > bestSavings[cid] {
+                            bestSavings[cid] = saving
+                        }
+                    } else {
+                        ch := grid[nr][nc]
+                        if isTrack(ch) && normalDistFromEnd[nr][nc] != -1 {
+                            remainder := normalDistFromEnd[nr][nc]
+                            cheatedTime := distStart + nsteps + remainder
+                            saving := normalTime - cheatedTime
+                            cid := cheatID{sr, sc, nr, nc}
+                            if saving > bestSavings[cid] {
+                                bestSavings[cid] = saving
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    count := 0
+    for _, s := range bestSavings {
+        if s >= 100 {
+            count++
+        }
+    }
+    return count
+}
+
 func bfs(grid []string, start, end point) int {
     visited := make(map[point]bool)
     queue := []struct {
@@ -22,7 +157,6 @@ func bfs(grid []string, start, end point) int {
     }{{start, 0}}
 
     visited[start] = true
-    dirs := []point{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
 
     for len(queue) > 0 {
         curr := queue[0]
@@ -68,11 +202,11 @@ func main() {
 				time := bfs(grid, startPoint, endPoint)
 				if originalTime - time >= 100 {
 					count++
-					// fmt.Println(time)
 				}
 				grid[row] = grid[row][:col] + "#" + grid[row][col+1:]
 			}
 		}
 	}
-	fmt.Println("count:", count)
+	fmt.Println("Part1:", count)
+    fmt.Println("Part 2:", solvePart2(grid, startPoint, endPoint))
 }
