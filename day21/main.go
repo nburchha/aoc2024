4215e24e52844d34f30993f8526a1e59d9d72694
nburchha <niklas@burchhardt.de>
2024-12-21 17:01:10 +0000

    new approach, part1 and 2 working (danke flo :))

--- day21/main.go
+++ day21/main.go
@@ -1,196 +1,189 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
+    "fmt"
+    "strconv"
 )
 
-var keyInit = Point{ row: 3, col: 2}
-var RobInit = Point{ row: 0, col: 1}
+var keypad = map[string][2]int{
+	"7": {0, 0}, "8": {0, 1}, "9": {0, 2},
+	"4": {1, 0}, "5": {1, 1}, "6": {1, 2},
+	"1": {2, 0}, "2": {2, 1}, "3": {2, 2},
+	"0": {3, 1}, "A": {3, 2},
 
-var keyMap = map[rune]int {
-	'1': 1,
-	'2': 2,
-	'3': 3,
-	'4': 4,
-	'5': 5,
-	'6': 6,
-	'7': 7,
-	'8': 8,
-	'9': 9,
-	'0': 0,
-	'A': -10,
-	'<': 0,
-	'V': 1,
-	'>': 2,
-	'^': 3,
+	"^": {0, 1}, "B": {0, 2},
+	"<": {1, 0}, "v": {1, 1}, ">": {1, 2},
 }
 
-var backwardsMap = map[int]rune {
-	0: '<',
-	1: 'v',
-	2: '>',
-	3: '^',
-	-10: 'A',
+var dirs = map[rune][2]int{
+	'^': {-1, 0},
+	'<': {0, -1},
+	'v': {1, 0},
+	'>': {0, 1},
 }
 
+type memoKey struct {
+    code       string
+    maxIter    int
+    depth      int
+}
+var memo = make(map[memoKey]int)
 
-type Point struct {
-	row, col int
+func permutationsOfString(s string) []string {
+	runes := []rune(s)
+	var results []string
+	seen := make(map[string]bool)
+
+	var generate func(int)
+	generate = func(start int) {
+		if start == len(runes)-1 {
+			perm := string(runes)
+			if !seen[perm] {
+				seen[perm] = true
+				results = append(results, perm)
+			}
+			return
+		}
+		for i := start; i < len(runes); i++ {
+			runes[start], runes[i] = runes[i], runes[start]
+			generate(start + 1)
+			runes[start], runes[i] = runes[i], runes[start] // backtrack
+		}
+	}
+
+	generate(0)
+	return results
 }
 
-var keypad = [][]int {
-	{7, 8, 9},
-	{4, 5, 6},		//
-	{1, 2, 3},		// 1, 2, 3
-	{-1, 0, -10},	// x, 0, A
+func checkmoves(start, end, illegal [2]int) []string {
+	dx := end[0] - start[0]
+	dy := end[1] - start[1]
+
+	movesNeeded := ""
+	if dx < 0 {
+		for i := 0; i < -dx; i++ {
+			movesNeeded += "^"
+		}
+	}
+	if dx > 0 {
+		for i := 0; i < dx; i++ {
+			movesNeeded += "v"
+		}
+	}
+	if dy < 0 {
+		for i := 0; i < -dy; i++ {
+			movesNeeded += "<"
+		}
+	}
+	if dy > 0 {
+		for i := 0; i < dy; i++ {
+			movesNeeded += ">"
+		}
+	}
+
+	perms := permutationsOfString(movesNeeded)
+
+	validMoves := []string{}
+Outer:
+	for _, perm := range perms {
+		pos := start
+		for _, step := range perm {
+			d := dirs[step]
+			pos = [2]int{pos[0] + d[0], pos[1] + d[1]}
+			if pos == illegal {
+				continue Outer
+			}
+		}
+		validMoves = append(validMoves, perm+"B")
+	}
+
+	if len(validMoves) == 0 {
+		return []string{"B"}
+	}
+	return validMoves
 }
 
-var rControl = [][]int { // TODO make this numeric too, so we can use same logic as for keypad
-	{-1, 3, -10},	// x, ^, A -> use abs(-10) to be able to continue with same logic
-	{0, 1, 2},		// <, V, >
-}
+func findPath(code string, maxiterations, depth int) int {
+	key := memoKey{code, maxiterations, depth}
+	if val, ok := memo[key]; ok {
+		return val
+	}
 
-func containsInRow(current, search int, slice [][]int) bool { // searches for the row where the current number is and looks if the search number is in the row
-	for row, _ := range slice {
-		for col, _ := range slice[row] {
-			if slice[row][col] == current {
-				for _, num := range slice[row] {
-					if num == search {
-						return true
-					}
+	var cur, illegal [2]int
+	if depth == 0 {
+		cur = keypad["A"]
+		illegal = [2]int{3, 0}
+	} else {
+		cur = keypad["B"]
+		illegal = [2]int{0, 0}
+	}
+
+	totalLength := 0
+	for _, ch := range code {
+		next := keypad[string(ch)]
+		allMoves := checkmoves(cur, next, illegal)
+
+		if depth == maxiterations {
+			totalLength += len(allMoves[0])
+		} else {
+			minVal := -1
+			for _, moveStr := range allMoves {
+				cost := findPath(moveStr, maxiterations, depth+1)
+				if minVal == -1 || cost < minVal {
+					minVal = cost
 				}
 			}
+			totalLength += minVal
 		}
+		cur = next
 	}
-	return false
-}
 
-func getPos(num int, keypad [][]int) Point {
-	for row := range keypad {
-		for col := range keypad[row] {
-			if keypad[row][col] == num {
-				return Point{row, col}
-			}
-		}
-	}
-	return Point{-1, -1}
-}
-
-func horizontal(pos, pos2 int, keypad [][]int) []int {
-	code := []int{}
-	posCoord := getPos(pos, keypad)
-	aCoord := getPos(pos2, keypad)
-	for posCoord.col != aCoord.col {
-		if posCoord.col > aCoord.col {
-			code = append(code, 0) // move left
-			posCoord.col--
-		} else {
-			code = append(code, 2) // move right
-			posCoord.col++
-		}
-	}
-	return code
-}
-
-func vertical(pos, pos2 int, keypad [][]int) []int {
-	code := []int{}
-	posCoord := getPos(pos, keypad)
-	aCoord := getPos(pos2, keypad)
-	for posCoord.row != aCoord.row {
-		if posCoord.row > aCoord.row {
-			code = append(code, 3) // move up
-			posCoord.row--
-		} else {
-			code = append(code, 1) // move down
-			posCoord.row++
-		}
-	}
-	return code
-}
-
-
-func findInstructions(pos, pos2 int, keypad [][]int) []int {
-    code := []int{}
-    if containsInRow(pos, -1, keypad) {
-		tmpCode := vertical(pos, pos2, keypad)
-		code = append(code, tmpCode...)
-		tmpCode = horizontal(pos, pos2, keypad)
-		code = append(code, tmpCode...)
-	} else {
-		tmpCode := horizontal(pos, pos2, keypad)
-		code = append(code, tmpCode...)
-		tmpCode = vertical(pos, pos2, keypad)
-		code = append(code, tmpCode...)
-	}
-	code = append(code, -10)
-    return code
-}
-
-func typeKeypad(instructions []int, keypad [][]int, stringInstructions string) []int {
-	pos := -10
-	code := []int{}
-	for _, instruction := range instructions {
-		code = append(code, findInstructions(pos, instruction, keypad)...)
-		pos = instruction
-	}
-	fmt.Print(stringInstructions, ": ")
-	for _, char := range code {
-		fmt.Print(string(backwardsMap[char]))
-	}
-	fmt.Println()
-	// return all possible combinations + check for -1
-	return code
-}
-
-func makeIntInstructions(code string) []int {
-	intInstructions := make([]int, 0)
-	for _, c := range code {
-		intInstructions = append(intInstructions, keyMap[c])
-	}
-	return intInstructions
+	memo[key] = totalLength
+	return totalLength
 }
 
 func main() {
-    codes := []string{
-        // "480A",
-        // "143A",
-        // "983A",
-        // "382A",
-        // "974A",
+	rawdata := []string{
+		// "480A",
+		// "143A",
+		// "983A",
+		// "382A",
+		// "974A",
 
 		"029A",
 		"980A",
 		"179A",
 		"456A",
 		"379A",
-    }
+	}
 
-	totalComplexity := 0
+	//part1 2 iterations
+	result := 0
+	for _, code := range rawdata {
+		if len(code) < 2 {
+			continue
+		}
+		numericPart := code[:len(code)-1]
+		numeric, err := strconv.Atoi(numericPart)
+		if err != nil {
+			continue
+		}
+		result += findPath(code, 2, 0) * numeric
+	}
+	fmt.Println(result)
 
-    for _, code := range codes {
-        // fmt.Println("Processing code:", code)
-
-        // Convert the code into instructions for the numeric keypad (Layer 1)
-        intInstructions := makeIntInstructions(code)
-        r1Instructions := typeKeypad(intInstructions, keypad, code)
-
-        // Generate instructions for Robot 1 controlling the numeric keypad (Layer 2)
-        r2Instructions := typeKeypad(r1Instructions, rControl, code)
-
-        // Generate instructions for Robot 2 controlling Robot 1 (Layer 3)
-        r3Instructions := typeKeypad(r2Instructions, rControl, code)
-
-        // Extract numeric part
-        numericPart, _ := strconv.Atoi(code[:len(code)-1]) // Remove 'A'
-
-        // Calculate complexity for this code
-        complexity := len(r3Instructions) * numericPart
-        totalComplexity += complexity
-
-        fmt.Printf("Code: %s, R3 Length: %d, Numeric: %d, Complexity: %d\n",
-            code, len(r3Instructions), numericPart, complexity)
-    }
-
-    fmt.Println("Total Complexity:", totalComplexity)
+	//part2 25 iterations
+	result = 0
+	for _, code := range rawdata {
+		if len(code) < 2 {
+			continue
+		}
+		numericPart := code[:len(code)-1]
+		numeric, err := strconv.Atoi(numericPart)
+		if err != nil {
+			continue
+		}
+		result += findPath(code, 25, 0) * numeric
+	}
+	fmt.Println(result)
 }
