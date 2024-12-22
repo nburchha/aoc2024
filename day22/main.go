cf885931337448152d4932ad08dcecc77d03c05d
nburchha <niklas@burchhardt.de>
2024-12-22 09:36:59 +0000

    part2 done, takes 10 mins or so LOL

--- day22/main.go
+++ day22/main.go
@@ -29,18 +29,40 @@ func evolveSecretNumber(secretNumber int) int {
 	return secretNumber
 }
 
+func calcBananas(sequence []int, prices [][]int) int {
+	var bananas = 0
+	for _, price := range prices {
+		for index := 0; index < len(price)-4; index++ {
+			flag := true
+			for i := 0; i < 4; i++ {
+				if sequence[i] != price[index+i+1] - price[index+i] {
+					flag = false
+					break
+				}
+			}
+			if flag {
+				bananas += price[index+4]
+				break
+			}
+		}
+	}
+	return bananas
+}
+
 func main() {
-	var secretNums []int
 	input, _ := os.ReadFile(FILE)
 	lines := strings.Split(string(input), "\n")
+	var secretNums []int
 	for _, line := range lines {
 		num, _ := strconv.Atoi(line)
 		secretNums = append(secretNums, num)
 	}
-
-	for i := 0; i < 2000; i++ {
-		for j, num := range secretNums {
-			secretNums[j] = evolveSecretNumber(num)
+	var prices [][]int
+	for j := range secretNums {
+		prices = append(prices, []int{})
+		for i := 0; i < 2000; i++ {
+			prices[j] = append(prices[j], (secretNums[j]%10))
+			secretNums[j] = evolveSecretNumber(secretNums[j])
 		}
 	}
 	sum := 0
@@ -48,4 +70,22 @@ func main() {
 		sum += num
 	}
 	fmt.Println(sum)
+
+	// Part 2
+	var bestPrice = 0
+	//try out each possible sequence
+	for i := -9; i < 10; i++ {
+		for j := -9; j < 10; j++ {
+			for k := -9; k < 10; k++ {
+				for l := -9; l < 10; l++ {
+					var sequence = []int{i, j, k, l}
+					tmp := calcBananas(sequence, prices)
+					if tmp > bestPrice {
+						bestPrice = tmp
+					}
+				}
+			}
+		}
+	}
+	fmt.Println(bestPrice)
 }
